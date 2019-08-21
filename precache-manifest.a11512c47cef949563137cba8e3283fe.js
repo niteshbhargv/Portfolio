33b84d8c73f420fa39d1b22a381cd12da960fe87
bhaveshbhargv <@bhavesh.bhargv@gamil.com>
2019-08-21 16:45:11 +0530

    Updates

---
+++ precache-manifest.a11512c47cef949563137cba8e3283fe.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ec45540a26ea71d8cf650b842586a59c",
+    "url": "/Personal/index.html"
+  },
+  {
+    "revision": "4db8ce05caed92d892b3",
+    "url": "/Personal/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "0fbabeb93e68157f6ae2",
+    "url": "/Personal/static/js/2.a541aa0c.chunk.js"
+  },
+  {
+    "revision": "4db8ce05caed92d892b3",
+    "url": "/Personal/static/js/main.23a9fdd6.chunk.js"
+  },
+  {
+    "revision": "798f2bf11102328ec457",
+    "url": "/Personal/static/js/runtime~main.025d3652.js"
+  }
+]);
